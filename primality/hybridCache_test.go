9fab1da4a8337c63fd4539b7b1ccc39defa3b3b6
Rufus Clark <rufusspclark@icloud.com>
2022-04-18 18:32:56 +0100

    added benchmarking for primality tests

--- primality/hybridCache_test.go
+++ primality/hybridCache_test.go
@@ -5,6 +5,31 @@ import (
 	"testing"
 )
 
+func BenchmarkHybridCache(b *testing.B) {
+	tests := []struct {
+		n    int
+		want bool
+	}{
+		{0, false},
+		{-10, false},
+		{2, true},
+		{11, true},
+		{97, true},
+		{100, false},
+		{10000, false},
+	}
+
+	for _, test := range tests {
+		benchname := fmt.Sprintf("HybridCache(%d)", test.n)
+
+		b.Run(benchname, func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				HybridCache(test.n)
+			}
+		})
+	}
+}
+
 func TestHybridCache(t *testing.T) {
 	tests := []struct {
 		n    int
